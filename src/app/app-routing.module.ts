b0155a202f2e5a655acce63d4f39e0d73780e419
Lucia Kokesova <lkokesova@gmail.com>
2021-12-10 17:05:16 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RoommatesComponent } from './roommates/roommates.component';
+import { ContactComponent } from './contact/contact.component';
+import { PhotoAlbumComponent } from './photo-album/photo-album.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: '/home', pathMatch: 'full' },
+  { path:'home', component: HomeComponent},
+  { path:'about', component: AboutComponent},
+  { path:'roommates', component: RoommatesComponent},
+  { path:'contact', component: ContactComponent},
+  { path:'photo-album', component: PhotoAlbumComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RoommatesComponent } from './roommates/roommates.component';
+import { ContactComponent } from './contact/contact.component'
+import { PhotoAlbumComponent } from './photo-album/photo-album.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutComponent,
+    RoommatesComponent,
+    ContactComponent,
+    PhotoAlbumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
